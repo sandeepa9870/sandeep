069645f7abdb2a844b307f5b1bcb1af3c770e01d
sandeepa9870 <sandeepalla772@gmail.com>
2026-03-17 19:33:49 +0530

    Add files via upload

---
+++ absSK.java
@@ -0,0 +1,85 @@
+import java.io.*;
+
+public class absSK{
+
+    public static void main(String[] args){
+
+        try{
+
+            FileReader file=new FileReader("text.txt");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("file:" +e);
+        }
+
+
+    }
+
+}
+
+
+
+
+
+//class notsureex extends Exception{
+//    public notsureex (String message){
+//        super(message);
+//
+//    }
+//
+//}
+//
+//public class abssk {
+//    public static void checkage(int age) throws notsureex {
+//        if (age < 18) {
+//            throw new notsureex("notsure");
+//        }
+//        System.out.println("sure");
+//    }
+//}
+//
+//public static void main(String[] args){
+//
+//    try {
+//        abssk.checkage(15);
+//    }
+//    catch (notsureex e) {
+//        System.out.println(e.getMessage());
+//    }
+//}
+//
+//void main() {
+//}
+
+
+
+
+
+
+//    }
+//    }
+//
+//       // try
+//        {
+//            int a=20;
+//        int b=10;
+//        int c=a/b;
+//        System.out.println(c);
+//        }
+//        catch (ArithmeticException e){
+//
+//        System.out.println("print the result:"+ e);
+//        }
+//
+//        finally {
+//            System.out.println("print the result:");
+//       /* }
+//
+//
+//
+//
+//    }
+//
+//}
+   //if you run the class we need to create child
+
